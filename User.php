033c5154424f956feab31abc468d8bcfa9b8ed34
mikijimbo13 <mikijimbo13@gmail.com>
2018-04-07 04:23:38 +0200

    Create User.php

---
+++ User.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class User{
+  private $name
+  private $mail
+  private $password
+  
+   
+  public function __construct($name, $mail, $passwordHash){
+    $this->name = $name;
+    $this->mail = $mail;
+    $this->pasword = $passwordHash  
+  }
+
+  public static function password_hash($password){
+    //do hash
+    return passwordHash;
+  }
+  
+  public function changePassword($newPassword){
+    
+  }
+  
+  
+
+}
