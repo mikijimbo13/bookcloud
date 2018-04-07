3fd7da9fab116430ad8746d23dcae42e1a812c36
mikijimbo13 <mikijimbo13@gmail.com>
2018-04-07 04:01:38 +0200

    Update and rename ItemVisitor.php to DisplayVisitor.php
    Change to searchable visitor

--- DisplayVisitor.php
+++ DisplayVisitor.php
@@ -6,9 +6,10 @@
  * Date: 27-Mar-18
  * Time: 16:12
  */
-abstract class ItemVisitor
+abstract class DisplayVisitor
 {
     abstract function visitBook(Book $book);
+    abstract function visitUser(User $user);
 }
 
 class DetailVisitor extends ItemVisitor
@@ -16,11 +17,17 @@ class DetailVisitor extends ItemVisitor
     function visitBook(Book $book){
         //html????
     }
+    function visitUser(User $user){
+        //html????
+    }
 }
 
 class SimpleVisitor extends ItemVisitor
 {
     function visitBook(Book $book){
+        //html????
+    }
+    function visitUser(User $user){
         //html????
     }
 }
